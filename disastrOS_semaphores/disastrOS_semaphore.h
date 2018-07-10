1b2c37afc1d8689e414d5d339a2e20c44370a5fa
PierfrancescoConti <pconti97@libero.it>
2018-07-10 15:41:16 +0200

    added functions to semaphore.h

--- disastrOS_semaphores/disastrOS_semaphore.h
+++ disastrOS_semaphores/disastrOS_semaphore.h
@@ -22,3 +22,9 @@ typedef ListHead SemaphoreList;
 Semaphore* SemaphoreList_byId(SemaphoreList* l, int id);
 
 void SemaphoreList_print(ListHead* l);
+
+
+void internal_semOpen();
+void internal_semClose();
+void internal_semWait();
+void internal_semPost();
